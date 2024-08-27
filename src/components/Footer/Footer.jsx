5fd0b52257fb1cdbc2dde5fe7040cf3cc8d0854a
Muaz Khalid <itzmajo786@gmail.com>
2024-08-27 18:33:29 +0500

    Home page done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,31 +6,32 @@ import wtsp from "../Assets/whatsapp_icon.png";
 import pin from "../Assets/pintester_icon.png";
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer-logo">
-        <img src={footer_logo} alt />
-        <p>SHOPPER</p>
-      </div>
+    <div>
+      <div className="footer">
+        <div className="footer-logo">
+          <img src={footer_logo} alt />
+          <p>SHOPPER</p>
+        </div>
 
-      <ul className="footer-links">
-        <li>Company</li>
-        <li>Products</li>
-        <li>Offices</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <div className="footer-social">
-        <div className="footer-icon">
-          <img src={insta} alt="Instagram" />
-        </div>
-        <div className="footer-icon">
-          <img src={wtsp} alt="Whatsapp" />
-        </div>
-        <div className="footer-icon">
-          <img src={pin} alt="Pintrest" />
+        <ul className="footer-links">
+          <li>Company</li>
+          <li>Products</li>
+          <li>Offices</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <div className="footer-social">
+          <div className="footer-icon">
+            <img src={insta} alt="Instagram" />
+          </div>
+          <div className="footer-icon">
+            <img src={wtsp} alt="Whatsapp" />
+          </div>
+          <div className="footer-icon">
+            <img src={pin} alt="Pintrest" />
+          </div>
         </div>
       </div>
-
       <div className="footer-copy">
         <hr />
         <p>Copyright @2024 - All Right Reserved.</p>
