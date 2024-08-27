c7f7df6544eadda6575a02f26efb3f157e022de8
Muaz Khalid <itzmajo786@gmail.com>
2024-08-27 18:17:42 +0500

    newsletter done

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -3,11 +3,13 @@ import "./NewLetter.css";
 const NewsLetter = () => {
   return (
     <div className="newsletter">
-      <h1>Get Exclusive Offers on Your Email</h1>
+      <h1 className="newsletter-h1">Get Exclusive Offers on Your Email</h1>
       <p>Subscribe to our newletter and stay updated</p>
       <div className="new-email">
         <input type="email" placeholder="Your Email" />
-        <button><b>Subscribe</b></button>
+        <button>
+          <b>Subscribe</b>
+        </button>
       </div>
     </div>
   );
